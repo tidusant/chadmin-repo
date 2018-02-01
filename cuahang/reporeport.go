2f65d1083ee76d9cf9ecda5f40a57800ae890fcd
duyhph <duyhph@gmail.com>
2018-02-01 18:42:34 +0700

    report by shipper

--- cuahang/reporeport.go
+++ cuahang/reporeport.go
@@ -10,7 +10,7 @@ import (
 	"gopkg.in/mgo.v2/bson"
 )
 
-func GetOrdersReportByRange(shopid string, start time.Time, end time.Time) models.Campaign {
+func GetOrdersReportByRange(shopid string, start, end time.Time, byship bool) models.Campaign {
 	col := db.C("addons_orders")
 	var rs models.Campaign
 	statusdetails := make(map[string]models.CampaignStatusDetail)
@@ -18,13 +18,21 @@ func GetOrdersReportByRange(shopid string, start time.Time, end time.Time) model
 	rpname := ""
 	andcond := []bson.M{}
 	if start.Year() > 1970 {
-		andcond = append(andcond, bson.M{"created": bson.M{"$gt": start.Unix()}})
+		if byship {
+			andcond = append(andcond, bson.M{"whookupdate": bson.M{"$gt": start.Unix()}})
+		} else {
+			andcond = append(andcond, bson.M{"created": bson.M{"$gt": start.Unix()}})
+		}
 		rpname = start.Format("02/01/2006") + " - "
 	} else {
 		rpname = "Start - "
 	}
 	if end.Year() > 1970 {
-		andcond = append(andcond, bson.M{"created": bson.M{"$lt": end.Unix()}})
+		if byship {
+			andcond = append(andcond, bson.M{"whookupdate": bson.M{"$lt": end.Unix()}})
+		} else {
+			andcond = append(andcond, bson.M{"created": bson.M{"$lt": end.Unix()}})
+		}
 		rpname += end.Format("02/01/2006")
 	} else {
 		rpname += "Now"
@@ -35,7 +43,7 @@ func GetOrdersReportByRange(shopid string, start time.Time, end time.Time) model
 
 	var ords []models.Order
 	err := col.Find(cond).All(&ords)
-	log.Debugf("start: %v\r\nend: %v\r\ncond: %v\r\nords: %v", start, end, cond, ords)
+	log.Debugf("start: %v\r\nend: %v\r\ncond: %v\r\nords: %v", start, end, cond)
 	stats := GetAllOrderStatus(shopid)
 	statsmap := make(map[string]models.OrderStatus)
 	for _, stat := range stats {
