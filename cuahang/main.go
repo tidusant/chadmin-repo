17ff662dba1957617825d65aa32795b9d0de523d
duy ha <duyhph@gmail.com>
2020-05-17 16:15:21 +0300

    slug function

--- cuahang/main.go
+++ cuahang/main.go
@@ -253,3 +253,26 @@ func Rebuild(usex models.UserSession) {
 	CreateCommonDataBuild(usex)
 
 }
+
+//==============slug=================
+func RemoveSlug(slug, shopid string) bool {
+	col := db.C("addons_slugs")
+	col.Remove(bson.M{"shopid": shopid, "slug": slug})
+	return true
+}
+func CreateSlug(slug, shopid, object string) bool {
+	col := db.C("addons_slugs")
+	col.Insert(bson.M{"shopid": shopid, "slug": slug, "object": object})
+	return true
+}
+func GetAllSlug(userid, shopid string) []string {
+	col := db.C("addons_slugs")
+	var rs []string
+	cond := bson.M{"shopid": shopid}
+	if userid != "594f665df54c58a2udfl54d3er" {
+		cond["userid"] = userid
+	}
+	err := col.Find(cond).Select(bson.M{"slug": 1}).All(&rs)
+	c3mcommon.CheckError("getallslug", err)
+	return rs
+}
